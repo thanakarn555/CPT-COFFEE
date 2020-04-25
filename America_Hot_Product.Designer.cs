1ce5fdb0119e84bd9611b1f1323f28535b0e733d
ChanonNon <52731328+ChanonNon@users.noreply.github.com>
2020-04-26 00:22:04 +0700

    add by Chanon

---
+++ America_Hot_Product.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Project_CoffeeShop
+{
+    partial class America_Hot_Product
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.AAAA = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(689, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(363, 393);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(181, 64);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // AAAA
+            // 
+            this.AAAA.AutoSize = true;
+            this.AAAA.Location = new System.Drawing.Point(76, 13);
+            this.AAAA.Name = "AAAA";
+            this.AAAA.Size = new System.Drawing.Size(46, 17);
+            this.AAAA.TabIndex = 2;
+            this.AAAA.Text = "label1";
+            // 
+            // America_Hot_Product
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Project_CoffeeShop.Properties.Resources.อเมริการ้อน;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(742, 503);
+            this.Controls.Add(this.AAAA);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "America_Hot_Product";
+            this.Text = "America_Hot_Product";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label AAAA;
+    }
+}
---
+++ America_Hot_Product.cs
@@ -0,0 +1,43 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_CoffeeShop
+{
+    public partial class America_Hot_Product : Form
+    {
+        public America_Hot_Product()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //int ID = int.Parse(textBox1.Text);
+            //string NAME = string.Format(textBox2.Text);
+            
+
+            string sql = " SELECT * FROM sales";
+            sql = " INSERT INTO sales (ProductName,Price) VALUES('America-Hot','20')";
+            MySqlConnection con = new MySqlConnection("host=localhost;user=root;password=123456789;database=coffee_project");
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            con.Open();
+
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+        }
+    }
+}
